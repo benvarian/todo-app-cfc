6c23dc50a639a9d3db8319a7c5aa6712c66776d5
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-01-07 15:02:44 +0800

    prettyy

---
+++ src/InputButton.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: ['clickFunction', 'content']
+}
+</script>
+
+<template>
+  <div class="w-1/3 m-auto">
+    <form @submit.prevent class="">
+      <button class="w-full h-full shadow-md rounded bg-blue-700 hover:bg-blue-600 button text-white text-sm px-5 py-2.5 mr-2 mb-2"
+          @click="clickFunction">{{ content }}</button>
+    </form>
+  </div>
+</template>
